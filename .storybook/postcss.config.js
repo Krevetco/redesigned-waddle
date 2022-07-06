ced94dea0787feef55b5bf6c9762ac7587069dc5
bbo <borya-hohlov@mail.ru>
2022-07-07 02:41:23 +0300

    remove jest config

--- .storybook/postcss.config.js
+++ .storybook/postcss.config.js
@@ -3,7 +3,7 @@ const WindiCSS = require('vite-plugin-windicss').default
 module.exports = {
     plugins: [
         require('postcss-import')(),
-        WindiCSS('./tailwind.config.js'), //This refers to your tailwind config
+        WindiCSS('./windicss.config.js'), //This refers to your tailwind config
         require('autoprefixer'),
     ],
 }
